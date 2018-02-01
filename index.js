6e6b81962c669df5046d8222ec4772f2322244aa
Zukhruf <zukhruf27@Hotmail.com>
2018-02-01 00:16:10 -0500

    seems like post/file upload is working good too

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var express = require('express')
 var app = express();
 var PNF = require('google-libphonenumber').PhoneNumberFormat;
 var phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
-app.get('/api/phonenumber/parse/text/:num', function(req, res){
+
+
+app.get('/api/phonenumbers/parse/text/:num', function(req, res){
     var msg = req.params.num;
     if (msg != "nothing" && msg != "") {
         //var arr = msg.split(',');
@@ -15,24 +19,39 @@ app.get('/api/phonenumber/parse/text/:num', function(req, res){
         res.status(200).json(parseNum(arr));
     } else {
         res.status(400).json([]);
-    }
-    
-    /*
-    var phoneNumber = phoneUtil.parse(msg, 'CA');
-    if (phoneUtil.isValidNumber(phoneNumber) == false) {
-        console.log("Invalid number supplied");
-      //  res.send("not a number");
-    } else  if (phoneUtil.isValidNumber(phoneNumber) == true) {
-        console.log(phoneUtil.format(phoneNumber, PNF.CANADA));
-       // res.send(phoneUtil.format(phoneNumber, PNF.CA));
-    }
-    */
-  //  let result = parseNum(req.params.num);
-  //  res.status(200).json(result);
-    
+    }    
 });
 
-//app.post
+app.get('/api/phonenumbers/parse/file/', async (req, res) => {
+    res.sendFile(__dirname + "/upload.html");
+});
+
+app.post('/api/phonenumbers/parse/file', upload.single('upload'), function(req, res) {
+    if (req.file) {
+        console.log("File located!");
+        var fs = require('fs');
+       // var buf = fs.readFileSync(req.file.path);
+       // var str = buf.toString('ascii');
+       // var buff = new Buffer(str, 'base64').toString('ascii');
+       var fileContents = fs.readFileSync(req.file.path);
+       var buf = Buffer.from(fileContents, 'base64').toString('ascii'); 
+       
+       var str = buf.split('\n') + " ";
+       // if (str != null && str != "") {
+            var arr = str.split(/[;,]/);
+       // } else {
+          //  res.status(400).send("File turned out to be empty!");
+        //}
+        console.log("Array length is " + arr.length);
+        res.status(200).json(parseNum(arr));
+    } else {
+        console.log("File not located!!!");
+        res.status(400).send("No file was uploaded!");
+    }
+
+
+});
+
 
 
 
@@ -41,19 +60,25 @@ function parseNum(num) {
     var number;
    // var numbers = phoneUtil.parse(text, "CA");
 
-    try {
+        console.log("inside Array length is " + num.length);
         for (var i = 0; i < num.length; i++) {
+
+
+    try {
+
             number = phoneUtil.parse(num[i], 'CA');
             if (phoneUtil.isValidNumber(number)) {
                 result.push(phoneUtil.format(number, PNF.NATIONAL));
             }
             
-        }
-    }
+ }
     catch(err) {
 
-        console.log("Exception for string: " + num[i] + " : " + err);
+    console.log("Exception for string: " + num[i] + " : " + err);
     }
+
+        }
+   
     return Array.from(new Set(result));
 }
 
